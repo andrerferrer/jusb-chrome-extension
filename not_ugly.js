60098926780547037fac5748cbc26349ed61f5ec
Andre Ferrer <andrerferrer@hotmail.com>
2022-01-03 19:49:43 -0300

    make jusb copy extension for Amandinha Eleninha

--- not_ugly.js
+++ not_ugly.js
@@ -1,45 +1,7 @@
-const filterTables = () => {
-	const quizzesBar = document.querySelector("body > div > div:nth-child(3) > div > div:nth-child(1)")
+const copyBtn = document.querySelector('.CopyContentModal-copyButton.btn.btn--md.btn--blue')
 
-	if (quizzesBar) {
-		const labelAndInput = `
-			<div class="m-3">
-				<label for="filter">Which status are you looking for?</label>
-				<select id="filter">
-				  <option value="All">All</option>
-				  <option value="Not Started">Not Started</option>
-				  <option value="Finished">Finished</option>
-				</select>
-			</div>
-		`
-
-		// Need to refactor this
-		const filterTables = (event) => {
-			const filterParams = event.currentTarget.value
-			const studentRows = document.querySelectorAll(".table tbody .quiz-row")
-			if (filterParams === 'Finished') {
-				studentRows.forEach((studentRow) => {
-					const started = studentRow.querySelector("td:nth-child(2)").innerText
-					studentRow.style.display = !/-/.test(started) ? "" : "none"
-				});
-			} else if (filterParams === 'All') {
-				studentRows.forEach((studentRow) => {
-					studentRow.style.display = "";
-				});
-			} else {
-				const regexp = new RegExp(filterParams, "i")
-				studentRows.forEach((studentRow) => {
-					const started = studentRow.querySelector("td.quiz-status").innerText
-					studentRow.style.display = regexp.test(started) ? "" : "none"
-				});
-			}
-		}
-
-
-		quizzesBar.insertAdjacentHTML('afterbegin', labelAndInput)
-		const input = document.getElementById('filter')
-		input.addEventListener('change', filterTables)
-	}
+if (copyBtn) {
+  copyBtn.addEventListener('click', () => {
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", document.querySelector("#app-root > div > div.portal-container > div > span > div > div.modal-dialog.modal-dialog-full > div > div.modal-body > div > div > div > div").innerText)
+  })
 }
-
-filterTables();
